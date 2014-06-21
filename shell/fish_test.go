4e80f4c2a2ede46a8daf97d522b76257a3c1b5bf
Alexander Kirillov <saratovsource@gmail.com>
2014-06-21 16:27:55 +0400

    Fix tests

--- shell/fish_test.go
+++ shell/fish_test.go
@@ -3,14 +3,14 @@ package shell
 import "testing"
 
 func TestDetechFish(t *testing.T) {
-	shell := DetechShell("fish")
+	shell := DetectShell("fish")
 	if shell == nil {
 		t.Errorf("Shell <%s> not found", shell)
 	}
 }
 
 func TestFishAlias(t *testing.T) {
-	shell := DetechShell("zsh")
+	shell := DetectShell("zsh")
 	original_command := "alias rails=\"bundle exec rails\";"
 	expected_command := shell.Alias("rails", "bundle exec rails")
 	if original_command != expected_command {
@@ -19,7 +19,7 @@ func TestFishAlias(t *testing.T) {
 }
 
 func TestFishUnalias(t *testing.T) {
-	shell := DetechShell("fish")
+	shell := DetectShell("fish")
 	original_command := "functions -e rails;"
 	expected_command := shell.Unalias("rails")
 	if original_command != expected_command {
